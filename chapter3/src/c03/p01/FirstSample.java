a89bfa60fb95c51106baffcb379c97fa8cd1bfb4
hfeng-air <harrifeng@gmail.com>
2013-12-28 15:41:50 +0800

    float number problem introduced

--- chapter3/src/c03/p01/FirstSample.java
+++ chapter3/src/c03/p01/FirstSample.java
@@ -3,5 +3,12 @@ package c03.p01;
 public class FirstSample {
     public static void main(String[] args) {
         System.out.println("We will not sue 'Hello, World!'");
+        ////////////////////////////////////////////////////////////////
+        // Not get 0.9, got 0.8999999999999999                        //
+        // Floating-point numbers are not suitable for financial      //
+        // calculations in which roundoff errors cannot be tolerated. //
+        // Use the BigDecimal class instead                           //
+        ////////////////////////////////////////////////////////////////
+        System.out.println(2.0 - 1.1);
     }
 }
