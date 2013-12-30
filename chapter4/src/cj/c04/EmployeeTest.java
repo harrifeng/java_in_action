0d112d66e6b7a949136b0ea308ccae011b76a3b4
hfeng <harrifeng@gmail.com>
2013-12-30 17:17:29 +0800

    use java to write class

---
+++ chapter4/src/cj/c04/EmployeeTest.java
@@ -0,0 +1,55 @@
+package cj.c04;
+
+import java.util.*;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee[] staff = new Employee[3];
+        staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 15);
+        staff[1] = new Employee("Harry hacker", 85000, 1989, 1, 15);
+        staff[2] = new Employee("Tony Tester", 45000, 1990, 3, 15);
+
+        for (Employee e: staff) {
+            e.raiseSalary(5);
+        }
+
+        for (Employee e : staff) {
+            System.out.println("name=" + e.getName() + ", salary=" +
+                               e.getSalary() + ", hireDay=" +
+                               e.getHireDay());
+        }
+
+    }
+}
+
+class Employee {
+    private String name;
+    private double salary;
+    private Date hireDay;
+
+    public Employee(String n, double s, int year, int month, int day) {
+        name = n;
+        salary = s;
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1,
+                                                           day);
+        // GregorianCalendar uses 0 for January
+        hireDay = calendar.getTime();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Date getHireDay() {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+}
