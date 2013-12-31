e08ef81e3a31f67f9658cecbf5851933dbaf8b33
hfeng <harrifeng@gmail.com>
2013-12-31 17:34:33 +0800

    seems work, test has to be aside with src folder

---
+++ test/oj/leet/lswrc/SolutionTest.java
@@ -0,0 +1,35 @@
+package test.oj.leet.lswrc; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import oj.leet.lswrc.*;
+
+/** 
+ * Solution Tester. 
+ * 
+ * @author <Authors name> 
+ * @version 1.0 
+ */ 
+public class SolutionTest { 
+
+    @Before
+    public void before() throws Exception { 
+    } 
+
+    @After
+    public void after() throws Exception { 
+    } 
+
+    /** 
+     * 
+     * Method: lengthOfLongestSubstring(String s) 
+     * 
+     */ 
+    @Test
+    public void testLengthOfLongestSubstring() throws Exception {
+        Solution solution = new Solution();
+        //TODO: Test goes here... 
+    } 
+
+} 
