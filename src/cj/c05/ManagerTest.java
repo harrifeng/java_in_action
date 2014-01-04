22b65256226128d5ee225ab25258daec896f0a56
mac.air.hfeng <harrifeng@gmail.com>
2014-01-04 09:58:23 +0800

    before abstract classes

--- src/cj/c05/ManagerTest.java
+++ src/cj/c05/ManagerTest.java
@@ -8,6 +8,14 @@ import cj.c04.Employee;
 public class ManagerTest {
     public static void main(String[] args) {
         Manager boss = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+
+        ////////////////////////////////////////////////
+        // but you can’t call                         //
+        // staff[0].setBonus(5000); // ERROR          //
+        // The declared type of staff[0] is Employee, //
+        // and the setBonus method is not a           //
+        // method of the Employee class.              //
+        ////////////////////////////////////////////////
         boss.setBonus(5000);
         Employee[] staff = new Employee[3];
         staff[0] = boss;
@@ -15,6 +23,9 @@ public class ManagerTest {
         staff[2] = new Employee("Tommy Tester", 40000, 1990, 3, 15);
 
         for (Employee e : staff) {
+            if (e instanceof Manager) {
+                System.out.printf(e.getName() + " is a Manager\n");
+            }
             System.out.println(e.getName() + " " + e.getSalary());
         }
 
