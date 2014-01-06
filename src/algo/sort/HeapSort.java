9318f8de1d2be04edbf273520608c766d098d7d9
hfeng <harrifeng@gmail.com>
2014-01-06 15:44:44 +0800

    heap sort in java

---
+++ src/algo/sort/HeapSort.java
@@ -0,0 +1,22 @@
+package algo.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by hfeng on 14-1-6.
+ */
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] aa = {11, 3, 5, 7, 2, 4, 6, 8, 16, 9, 1, 12, 10, 13, 14, 15, 17 };
+        System.out.println(Arrays.toString(aa));
+
+        MinHeap mih = new MinHeap(aa);
+        mih.heapSort();
+        System.out.println(Arrays.toString(mih.getArray()));
+
+        MaxHeap mah = new MaxHeap(aa);
+        mah.heapSort();
+        System.out.println(Arrays.toString(mah.getArray()));
+
+    }
+}
---
+++ src/algo/sort/MinHeap.java
@@ -0,0 +1,43 @@
+package algo.sort;
+
+/**
+ * Created by hfeng on 14-1-6.
+ */
+
+
+public class MinHeap extends Heap {
+    public MinHeap(int[] a) {
+        super(a);
+    }
+
+    public void minHeap(int root, int end) {
+        int left = root * 2 + 1;
+        int right = root * 2 + 2;
+        int min = root;
+
+        if (left <= end && getArray()[left] < getArray()[root]) {
+            min = left;
+        }
+        if (right <= end && getArray()[right] < getArray()[min]) {
+            min = right;
+        }
+
+        if (min != root) {
+            swapPos(min, root);
+            minHeap(min, end);
+        }
+    }
+    public void buildMinHeap() {
+        for (int i = getLen() / 2  ; i >= 0; i--) {
+            minHeap(i, getLen() - 1);
+        }
+    }
+
+    public void heapSort() {
+        buildMinHeap();
+        for (int i = getLen() - 1; i >= 1; i--) {
+            swapPos(0, i);
+            minHeap(0, i - 1);
+        }
+    }
+}
