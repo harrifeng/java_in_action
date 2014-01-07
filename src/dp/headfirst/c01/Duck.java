71dce138d7d5dae7c3bfe6eef1d3157c0c2db888
harrifeng <harrifeng@gmail.com>
2014-01-07 22:41:48 +0800

    [dp][c01_01] It started with simple SimUDuck app

---
+++ src/dp/headfirst/c01/Duck.java
@@ -0,0 +1,25 @@
+package dp.headfirst.c01;
+
+/**
+ * Created by hfeng on 1/7/14.
+ */
+public abstract class Duck {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Duck(String n) {
+        this.name = n;
+    }
+
+    public void quack() {
+        System.out.println("quack");
+    }
+    public void swim() {
+        System.out.println("swim");
+    }
+
+    public abstract void display();
+}
---
+++ src/dp/headfirst/c01/MallardDuck.java
@@ -0,0 +1,13 @@
+package dp.headfirst.c01;
+
+/**
+ * Created by hfeng on 1/7/14.
+ */
+public class MallardDuck extends Duck{
+    public MallardDuck() {
+        super("green head");
+    }
+    public void display() {
+        System.out.println("Looks like a mallard");
+    }
+}
