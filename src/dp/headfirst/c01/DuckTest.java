695253c2dde1a6867c8e314c3dd6aa72403f392b
harrifeng <harrifeng@gmail.com>
2014-01-07 23:13:23 +0800

    [dp][c01_03] But something went horribly wrong...

---
+++ src/dp/headfirst/c01/DuckTest.java
@@ -0,0 +1,13 @@
+package dp.headfirst.c01;
+
+/**
+ * Created by hfeng on 1/7/14.
+ */
+public class DuckTest {
+    public static void main(String[] args) {
+        RubberDuck rb = new RubberDuck();
+        rb.quack();
+        rb.display();
+        rb.fly();  //oh, no it can not fly
+    }
+}
