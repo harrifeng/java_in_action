fe4f210534dedd372204c57da38d521f4b8a5f02
hfeng <harrifeng@gmail.com>
2014-01-09 20:58:50 +0800

    begin regular express

---
+++ test/oj/leet/rem/Solution.java
@@ -0,0 +1,10 @@
+package oj.leet.rem;
+
+/**
+ * Created by hfeng on 14-1-9.
+ */
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        return false;
+    }
+}
---
+++ test/oj/leet/rem/SolutionTest.java
@@ -0,0 +1,38 @@
+package oj.leet.rem;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * Solution Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ */
+public class SolutionTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: isMatch(String s, String p)
+     */
+    @Test
+    public void testIsMatch() throws Exception {
+        Solution solution = new Solution();
+        org.junit.Assert.assertFalse(solution.isMatch("aa", "a"));
+        org.junit.Assert.assertFalse(solution.isMatch("aaa", "a"));
+
+        org.junit.Assert.assertTrue(solution.isMatch("aa", "aa"));
+        org.junit.Assert.assertTrue(solution.isMatch("aa", "a*"));
+        org.junit.Assert.assertTrue(solution.isMatch("ab", ".*"));
+        org.junit.Assert.assertTrue(solution.isMatch("aab", "c*a*b"));
+
+    }
+}
