1eddbffbac03ce44392544c03184a8ccf215ede2
hfeng <harrifeng@gmail.com>
2014-03-03 16:46:43 +0800

    Strbuzz example

---
+++ src/dp/headfirst/c03/p01/CondimentDecorator.java
@@ -0,0 +1,5 @@
+package dp.headfirst.c03.p01;
+
+public abstract class CondimentDecorator extends Beverage{
+    public abstract String getDescription();
+}
---
+++ src/dp/headfirst/c03/p01/Espresso.java
@@ -0,0 +1,11 @@
+package dp.headfirst.c03.p01;
+
+public class Espresso extends Beverage{
+    public Espresso() {
+        description = "Espresso";
+    }
+
+    public double cost() {
+        return 1.99;
+    }
+}
