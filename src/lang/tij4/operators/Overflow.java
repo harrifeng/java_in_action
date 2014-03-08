540fc8d7f3eb546c014f2fc6da106ca55e2a6a24
hfeng.air <harrifeng@gmail.com>
2014-03-08 20:03:15 +0800

    finish chapter3

---
+++ src/lang/tij4/operators/Overflow.java
@@ -0,0 +1,16 @@
+package lang.tij4.operators;
+
+public class Overflow {
+    public static void main(String[] args) {
+        int big = Integer.MAX_VALUE;
+        System.out.println("big = " + big);
+        int bigger = big * 4;
+        System.out.println("bigger = " + bigger);
+    }
+}
+
+//////////////////////
+// Output===>       //
+// big = 2147483647 //
+// bigger = -4      //
+//////////////////////
