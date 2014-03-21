9972e0e94ff2a4ab589743c0cd915637d907776f
hfeng.air <harrifeng@gmail.com>
2014-03-21 23:40:40 +0800

    new way to solve this problem, may be better

--- src/oj/leet/lcp/Solution.java
+++ src/oj/leet/lcp/Solution.java
@@ -1,26 +1,22 @@
 package oj.leet.lcp;
-
-/**
- * Created by hfeng on 14-1-22.
- */
-
 public class Solution {
     public String longestCommonPrefix(String[] strs){
-        int slen = strs.length;
-        if (slen == 0) {
-            return "";
-        }
-        int commonLen = 0;
-        for (int i = 0; i < strs[0].length(); i++) {
-            char pivot = strs[0].charAt(commonLen);
-            for (int j = 1; j < slen; j++) {
-                if (strs[j].length() <= commonLen || strs[j].charAt(commonLen) != pivot) {
-                    return strs[0].substring(0, commonLen);
-                }
-            }
-            commonLen++;
+        StringBuffer sbf = new StringBuffer("");
+        if (strs.length == 0) {
+            return sbf.toString();
         }
 
-        return strs[0];
+        for (int i = 0; i < strs[0].length(); i++) {
+            char target = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].length() <= i) {
+                    return sbf.toString();
+                } else if (strs[j].charAt(i) != target) {
+                    return sbf.toString();
+                }
+            }
+            sbf.append(target);
+        }
+        return sbf.toString();
     }
 }
