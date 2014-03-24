31234e5d451f19973d964f78f162573ce0206182
hfeng-desk <harrifeng@gmail.com>
2014-03-24 22:51:50 +0800

    use gaodashang hamcrest collection to test ArrayList

--- test/oj/leet/ths/SolutionTest.java
+++ test/oj/leet/ths/SolutionTest.java
@@ -8,7 +8,9 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 
 public class SolutionTest {
 
@@ -26,74 +28,70 @@ public class SolutionTest {
     @Test
     public void testThreeSum() throws Exception {
         Solution solution = new Solution();
-        Set<ArrayList<Integer>> result1 = new HashSet<ArrayList<Integer>>();
 
-        int[] a1 = {0, 0, 0, 0};
+        int num1[] = {0, 0, 0, 0};
+        ArrayList<Integer> a1 = new ArrayList<Integer>();
+        a1.add(0);
+        a1.add(0);
+        a1.add(0);
+        ArrayList<ArrayList<Integer>> expected1 = new ArrayList<ArrayList<Integer>>();
+        expected1.add(a1);
+        assertThat("", expected1, containsInAnyOrder(solution.threeSum(num1).toArray()));
 
-        ArrayList<Integer> al1 = new ArrayList<Integer>();
-        al1.add(0);
-        al1.add(0);
-        al1.add(0);
-        result1.add(al1);
-        assertEquals(result1, new HashSet<ArrayList<Integer>>(solution.threeSum(a1)));
-        result1.clear();
+        int num2[] = {-1, 0, 1, 2, -1, -4};
+        ArrayList<Integer> a2 = new ArrayList<Integer>();
+        ArrayList<Integer> a20 = new ArrayList<Integer>();
+        a2.add(-1);
+        a2.add(0);
+        a2.add(1);
+        a20.add(-1);
+        a20.add(-1);
+        a20.add(2);
+        ArrayList<ArrayList<Integer>> expected2 = new ArrayList<ArrayList<Integer>>();
+        expected2.add(a2);
+        expected2.add(a20);
+        assertThat("", expected2, containsInAnyOrder(solution.threeSum(num2).toArray()));
 
+        int num3[] = {-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6};
+        ArrayList<Integer> a3 = new ArrayList<Integer>();
+        ArrayList<Integer> a30 = new ArrayList<Integer>();
+        ArrayList<Integer> a31 = new ArrayList<Integer>();
+        ArrayList<Integer> a32 = new ArrayList<Integer>();
+        ArrayList<Integer> a33 = new ArrayList<Integer>();
+        ArrayList<Integer> a34 = new ArrayList<Integer>();
+        a3.add(-4);
+        a3.add(-2);
+        a3.add(6);
 
+        a30.add(-4);
+        a30.add(0);
+        a30.add(4);
 
-        int[] a2 = {-1, 0, 1, 2, -1, -4};
-        ArrayList<Integer> al2 = new ArrayList<Integer>();
-        ArrayList<Integer> al3 = new ArrayList<Integer>();
-        ArrayList<ArrayList<Integer>> alal = new ArrayList<ArrayList<Integer>>();
-        ArrayList<ArrayList<Integer>> soal = new ArrayList<ArrayList<Integer>>();
+        a31.add(-4);
+        a31.add(1);
+        a31.add(3);
 
-        al2.add(-1);
-        al2.add(0);
-        al2.add(1);
+        a32.add(-4);
+        a32.add(2);
+        a32.add(2);
 
-        al3.add(-1);
-        al3.add(-1);
-        al3.add(2);
+        a33.add(-2);
+        a33.add(-2);
+        a33.add(4);
 
-        result1.add(al2);
-        result1.add(al3);
+        a34.add(-2);
+        a34.add(0);
+        a34.add(2);
 
-        alal.add(al2);
-        alal.add(al3);
+        ArrayList<ArrayList<Integer>> expected3 = new ArrayList<ArrayList<Integer>>();
+        expected3.add(a3);
+        expected3.add(a30);
+        expected3.add(a31);
+        expected3.add(a32);
+        expected3.add(a33);
+        expected3.add(a34);
 
-        soal = solution.threeSum(a2);
+        assertThat("", expected3, containsInAnyOrder(solution.threeSum(num3).toArray()));
 
-        System.out.println("------------");
-        System.out.println("result is  :" + soal);
-        System.out.println("expected is:" + alal);
-
-        soal.removeAll(alal);
-        alal.removeAll(solution.threeSum(a2));
-
-        org.junit.Assert.assertEquals(soal, alal);
-
-        int[] a4 = {-2,0,0,2,2};
-        ArrayList<Integer> al4 = new ArrayList<Integer>();
-
-        al4.add(-2);
-        al4.add(0);
-        al4.add(2);
-
-        result1.add(al4);
-
-        alal.clear();
-        soal.clear();
-
-        alal.add(al4);
-        soal = solution.threeSum(a4);
-
-        System.out.println("------------");
-        System.out.println("result is  :" + soal);
-        System.out.println("expected is:" + alal);
-
-        assertEquals(alal.size(), soal.size());
-        soal.removeAll(alal);
-        alal.removeAll(solution.threeSum(a4));
-
-        assertEquals(soal, alal);
     }
 }
