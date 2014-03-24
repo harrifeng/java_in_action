8aa9038940010142a3bdc1ac6383cbfb3c4dc8f3
harrifeng <harrifeng@gmail.com>
2014-03-24 19:10:31 +0800

    ready for 3 sum to blog

--- src/oj/leet/ths/Solution.java
+++ src/oj/leet/ths/Solution.java
@@ -4,39 +4,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Solution {
+
     public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
         ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
-        if (num.length < 3) {
-            return ret;
-        }
+
         Arrays.sort(num);
         for (int i = 0; i < num.length; i++) {
+            // should be while until it is not equal
             while (i > 0 && i < num.length && num[i] == num[i - 1]) {
                 i++;
             }
-            if (i >= num.length - 2) {
-                break;
-            }
-            int target = num[i] * (-1);
+
+            // No need to check whether i is equal(or bigger than) num.length - 2,
+            // in the equal case, beg == num.length - 1, which equals end, no loop
+            // will happen
             int beg = i + 1;
             int end = num.length - 1;
+
             while (beg < end) {
                 int sum = num[beg] + num[end];
-                if (sum == target) {
+                if (num[i] + sum == 0) {
                     ArrayList<Integer> oneRet = new ArrayList<Integer>();
                     oneRet.add(num[i]);
                     oneRet.add(num[beg]);
                     oneRet.add(num[end]);
                     ret.add(oneRet);
-                    while (beg < num.length - 2 && num[beg] == num[beg+1]) {
-                        ++beg;
+                    // should be while until it is not equal
+                    // only duplicated if both beg and end are equal with their neighbor
+                    while (beg < end && num[beg] == num[beg + 1] && num[end] == num[end - 1]) {
+                        beg++;
+                        end--;
                     }
-                    ++beg;
-                    while (end > 1 && num[end] == num[end-1]) {
-                        --end;
-                    }
-                    --end;
-                } else if (sum < target) {
+                    beg++;
+                    end--;
+                } else if (num[i] + sum < 0) {
                     beg++;
                 } else {
                     end--;
