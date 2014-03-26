862dcdebb33de9fee4b81a19846933432b8d7ee8
Harri Feng <harrifeng@gmail.com>
2014-03-26 15:37:15 +0800

    Use hamcrest to test collection

--- test/oj/leet/fs/SolutionTest.java
+++ test/oj/leet/fs/SolutionTest.java
@@ -6,12 +6,10 @@ import org.junit.After;
 
 import java.util.ArrayList;
 
-/**
- * Solution Tester.
- *
- * @author <Authors name>
- * @version 1.0
- */
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+
+
 public class SolutionTest {
 
     @Before
@@ -29,51 +27,68 @@ public class SolutionTest {
     public void testFourSum() throws Exception {
 
         Solution solution = new Solution();
+        int num1[] = {1, 0, -1, 0, -2, 2};
+        int target1 = 0;
+        ArrayList<Integer> a1 = new ArrayList<Integer>();
+        a1.add(-1);
+        a1.add(0);
+        a1.add(0);
+        a1.add(1);
+        ArrayList<Integer> a11 = new ArrayList<Integer>();
+        a11.add(-2);
+        a11.add(-1);
+        a11.add(1);
+        a11.add(2);
+        ArrayList<Integer> a12 = new ArrayList<Integer>();
+        a12.add(-2);
+        a12.add(0);
+        a12.add(0);
+        a12.add(2);
 
-        int[] a1 = {1, 0, -1, 0, -2, 2};
-        int t1 = 0;
-        ArrayList<Integer> al1 = new ArrayList<Integer>();
-        ArrayList<Integer> al2 = new ArrayList<Integer>();
-        ArrayList<Integer> al3 = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>> expected1 = new ArrayList<ArrayList<Integer>>();
+        expected1.add(a1);
+        expected1.add(a11);
+        expected1.add(a12);
+        System.out.println("expected: " + expected1);
+        System.out.println("actually: " + solution.fourSum(num1, target1));
+        assertThat("ArrayList equal", expected1, containsInAnyOrder(
+            solution.fourSum(num1, target1).toArray()));
+        ////////////
+        // TEST 2 //
+        ////////////
+        int num2[] = {-1, 0, 1, 2, -1, 4};
+        int target2 = -1;
+        ArrayList<Integer> a2 = new ArrayList<Integer>();
+        a2.add(-1);
+        a2.add(-1);
+        a2.add(0);
+        a2.add(1);
+        
+        ArrayList<ArrayList<Integer>> expected2 = new ArrayList<ArrayList<Integer>>();
+        expected2.add(a2);
+        System.out.println("expected: " + expected2);
+        System.out.println("actually: " + solution.fourSum(num2, target2));
+        assertThat("ArrayList equal", expected2, containsInAnyOrder(
+            solution.fourSum(num2, target2).toArray()));
 
-        ArrayList<ArrayList<Integer>> alal = new ArrayList<ArrayList<Integer>>();
-        ArrayList<ArrayList<Integer>> soal = new ArrayList<ArrayList<Integer>>();
+        ////////////
+        // TEST 3 //
+        ////////////
+        int num3[] = {-3, -1, 0, 2, 4, 5};
+        int target3 = 2;
+        ArrayList<Integer> a3 = new ArrayList<Integer>();
+        a3.add(-3);
+        a3.add(-1);
+        a3.add(2);
+        a3.add(4);
+        
+        ArrayList<ArrayList<Integer>> expected3 = new ArrayList<ArrayList<Integer>>();
+        expected3.add(a3);
+        System.out.println("expected: " + expected3);
+        System.out.println("actually: " + solution.fourSum(num3, target3));
+        assertThat("ArrayList equal", expected3, containsInAnyOrder(
+            solution.fourSum(num3, target3).toArray()));
 
-        al1.add(-1);
-        al1.add(0);
-        al1.add(0);
-        al1.add(1);
-
-        al2.add(-2);
-        al2.add(-1);
-        al2.add(1);
-        al2.add(2);
-
-        al3.add(-2);
-        al3.add(0);
-        al3.add(0);
-        al3.add(2);
-
-        alal.add(al1);
-        alal.add(al2);
-        alal.add(al3);
-
-        soal = solution.fourSum(a1, t1);
-
-
-        System.out.println("------------");
-        System.out.println("result is  :" + soal);
-        System.out.println("expected is:" + alal);
-
-        soal.removeAll(alal);
-        alal.removeAll(solution.fourSum(a1, t1));
-
-        System.out.println("------------");
-        System.out.println("result is  :" + soal);
-        System.out.println("expected is:" + alal);
-
-        org.junit.Assert.assertEquals(soal, alal);
+        
     }
-
-
-} 
+}
