ed846dbcdae57514c3ff86ec2b72fa9392c5b6cf
hfeng.air <harrifeng@gmail.com>
2014-03-26 22:31:21 +0800

    same way to resolve any Xsum question

--- src/oj/leet/tsc/Solution.java
+++ src/oj/leet/tsc/Solution.java
@@ -2,34 +2,36 @@ package oj.leet.tsc;
 
 import java.util.Arrays;
 
-/**
- * Created by hfeng on 14-1-27.
- */
 public class Solution {
     public int threeSumClosest(int[] num, int target) {
-        int minV = Integer.MAX_VALUE;
-        int ret = Integer.MAX_VALUE;
         Arrays.sort(num);
-
-        for (int i = 0; i + 2 < num.length; i++) {
+        int minV = Integer.MAX_VALUE;
+        if (num.length < 3) {
+            return minV;
+        }
+        int minSum = Integer.MAX_VALUE;
+        for (int i = 0; i < num.length; i++) {
+            if (i > 0 && num[i] == num[i - 1]) {
+                continue;
+            }
             int beg = i + 1;
             int end = num.length - 1;
             while (beg < end) {
                 int sum = num[i] + num[beg] + num[end];
-                int newMin = Math.abs(sum - target);
 
-                if (newMin < minV) {
-                    minV = newMin;
-                    ret = sum;
-                }
-
-                if (sum - target > 0) {
-                    end--;
-                } else {
+                if (sum == target) {
+                    return target;
+                } else if (sum < target) {
                     beg++;
+                } else {
+                    end--;
+                }
+                if (minV > Math.abs(sum - target)) {
+                    minV = Math.abs(sum - target);
+                    minSum = sum;
                 }
             }
         }
-        return ret;
+        return minSum;
     }
 }
