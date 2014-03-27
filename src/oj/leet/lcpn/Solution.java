ff855c6855228a760197315070d7a97bfef22bdd
hfeng.air <harrifeng@gmail.com>
2014-03-27 23:14:21 +0800

    java can not have const String array

--- src/oj/leet/lcpn/Solution.java
+++ src/oj/leet/lcpn/Solution.java
@@ -2,30 +2,28 @@ package oj.leet.lcpn;
 
 import java.util.ArrayList;
 
-/**
- * Created by hfeng on 1/18/14.
- */
 public class Solution {
-    private String[] ARR = {"", "", "abc", "def", "ghi", "jkl",
-            "mno", "pqrs", "tuv", "wxyz"};
-
-    private void letCom(String digits, int level, StringBuffer tmp, ArrayList<String> result) {
-        if (level == digits.length()) {
-            result.add(tmp.toString());
-            return;
-        }
-        String currStr = ARR[digits.charAt(level) - '0'];
-        for (int i = 0; i < currStr.length(); i++) {
-            tmp.append(currStr.charAt(i));
-            letCom(digits, level + 1, tmp, result);
-            tmp.deleteCharAt(tmp.length()-1);
-        }
+    public ArrayList<String> letterCombinations(String digits) {
+        ArrayList<String> ret = new ArrayList<String>();
+        StringBuffer tmpString = new StringBuffer();
+        letterCom(digits, 0, tmpString, ret);
+        return ret;
     }
 
-    public ArrayList<String> letterCombinations(String digits) {
-        ArrayList<String> al = new ArrayList<String>();
-        StringBuffer sb = new StringBuffer();
-        letCom(digits, 0, sb, al);
-        return al;
+    private String[] num_letter = {"", "", "abc", "def", "ghi", "jkl",
+            "mno", "pqrs", "tuv", "wxyz"};
+
+    private void letterCom(String digits, int level, StringBuffer tmpS,
+                           ArrayList<String> result) {
+        if (level == digits.length()) {
+            result.add(tmpS.toString());
+            return;
+        }
+        String nowStr = num_letter[digits.charAt(level) - '0'];
+        for (int i = 0; i < nowStr.length(); i++) {
+            tmpS.append(nowStr.charAt(i));
+            letterCom(digits, level + 1, tmpS, result);
+            tmpS.deleteCharAt(tmpS.length() - 1);
+        }
     }
 }
--- src/tmp/util/TestCode.java
+++ src/tmp/util/TestCode.java
@@ -3,10 +3,9 @@ package tmp.util;
 import java.util.Stack;
 
 public class TestCode {
-
+    private final static String[] test = {"abc", "def"};
     public static void main(String[] args) {
-        Stack s = new Stack();
-        s.addElement('a');
-        System.out.println(s);
+        test[0] = "fhr";
+        System.out.println(test[0]);
     }
 }
