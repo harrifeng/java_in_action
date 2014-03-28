0795cfeb7b70f67ff6f885b9ece2ca6bbc3da367
hfeng.desktop <harrifeng@gmail.com>
2014-03-28 21:10:20 +0800

    changes added

---
+++ src/lang/tij4/interfaces/filters/Filter.java
@@ -0,0 +1,11 @@
+package lang.tij4.interfaces.filters;
+
+public class Filter {
+    public String name() {
+        return getClass().getSimpleName();
+    }
+    public Waveform process(Waveform input) {
+        return input;
+    }
+
+}
---
+++ src/lang/tij4/interfaces/filters/Waveform.java
@@ -0,0 +1,10 @@
+package lang.tij4.interfaces.filters;
+
+public class Waveform {
+    private static long counter;
+    private final long id = counter++;
+    public String toString() {
+        return "Waveform " + id;
+    }
+
+}
