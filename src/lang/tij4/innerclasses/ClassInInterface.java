623f10748e157307a5c574497f355e08b8638fb8
hfeng.air <harrifeng@gmail.com>
2014-04-03 22:09:32 +0800

    new incoming before Why

---
+++ src/lang/tij4/innerclasses/ClassInInterface.java
@@ -0,0 +1,22 @@
+package lang.tij4.innerclasses;
+
+public interface ClassInInterface {
+    void howdy();
+    class Test implements ClassInInterface {
+        public void howdy() {
+            System.out.println("Howdy!");
+        }
+        public static void main(String[] args) {
+            new Test().howdy();
+        }
+    }
+}
+/////////////////////////////////////////////////////////////
+// bash-3.2$ ./runj.sh innerclasses/ClassInInterface\$Test //
+// --------Begin----------                                 //
+//                                                         //
+// <===================OUTPUT===================>          //
+// Howdy!                                                  //
+//                                                         //
+// --------End------------                                 //
+/////////////////////////////////////////////////////////////
