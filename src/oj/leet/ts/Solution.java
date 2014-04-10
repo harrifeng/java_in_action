624b6d1971842d123496c1e567483c9c3cb6085b
hfeng.air <harrifeng@gmail.com>
2014-04-10 16:56:45 +0800

    Two sum repeated

--- src/oj/leet/ts/Solution.java
+++ src/oj/leet/ts/Solution.java
@@ -2,23 +2,20 @@ package oj.leet.ts;
 
 import java.util.HashMap;
 
-/**
- * Created by hfeng on 14-1-3.
- */
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int n = numbers.length;
-        int[] result = new int[2];
+
         for (int i = 0; i < numbers.length; i++) {
             if (map.containsKey(target - numbers[i])) {
-                result[0] = map.get(target - numbers[i]) + 1;
-                result[1] = i + 1;
-                break;
+                int[] ret = new int[2];
+                ret[0] = map.get(target - numbers[i]) + 1;
+                ret[1] = i + 1;
+                return ret;
             } else {
                 map.put(numbers[i], i);
             }
         }
-        return result;
+        return null;
     }
 }
