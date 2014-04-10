dc2386e1db43c5e28aa02f45a945046fe2efa6f5
hfeng.air <harrifeng@gmail.com>
2014-04-10 22:22:40 +0800

    Median of Two Sorted Array repeated

--- src/oj/leet/mtsa/Solution.java
+++ src/oj/leet/mtsa/Solution.java
@@ -2,15 +2,11 @@ package oj.leet.mtsa;
 
 import java.util.Arrays;
 
-import static java.lang.Math.min;
-
-/**
- * Created by hfeng on 13-12-31.
- */
 public class Solution {
-    private int findNth(int A[], int B[], int k){
+
+    public int findKthSortedArrays(int[] A, int[] B, int k) {
         if (A.length > B.length) {
-            return findNth(B, A, k);
+            return findKthSortedArrays(B, A, k);
         }
         if (A.length == 0) {
             return B[k - 1];
@@ -21,22 +17,22 @@ public class Solution {
         if (k == 1) {
             return Math.min(A[0], B[0]);
         }
-        int partA = Math.min(k/2, A.length);
+        int partA = Math.min(k / 2, A.length);
         int partB = k - partA;
-
         if (A[partA - 1] < B[partB - 1]) {
-            return findNth(Arrays.copyOfRange(A, partA, A.length), B, k - partA);
+            return findKthSortedArrays(Arrays.copyOfRange(A, partA, A.length), B, k - partA);
         } else {
-            return findNth(A, Arrays.copyOfRange(B, partB, B.length), k - partB);
+            return findKthSortedArrays(A, Arrays.copyOfRange(B, partB, B.length), k - partB);
         }
     }
 
     public double findMedianSortedArrays(int A[], int B[]) {
-        int lenSum = A.length + B.length;
-        if (lenSum % 2 == 1) {
-            return findNth(A, B, lenSum / 2 + 1);
+        int sum = A.length + B.length;
+        if (sum % 2 == 0) {
+            return (findKthSortedArrays(A, B, sum / 2) +
+                    findKthSortedArrays(A, B, sum / 2 + 1)) / 2.0;
         } else {
-            return (findNth(A, B, lenSum / 2) + findNth(A, B, lenSum / 2 + 1)) / 2.0;
+            return findKthSortedArrays(A, B, sum / 2 + 1);
         }
     }
 }
