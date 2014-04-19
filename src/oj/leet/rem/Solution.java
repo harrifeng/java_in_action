edcfaeb93920dd1f14167d94d195fa3e5a45d745
hfeng.air <harrifeng@gmail.com>
2014-04-19 17:03:25 +0800

    regular expression repeated, really understand it.

--- src/oj/leet/rem/Solution.java
+++ src/oj/leet/rem/Solution.java
@@ -1,35 +1,35 @@
 package oj.leet.rem;
+
 public class Solution {
     public boolean isMatch(String s, String p) {
         if (p.length() == 0) {
             return s.length() == 0;
         }
-
-        // length == 1 is the case that is easy to forget.
-        // as p is subtracted 2 each time, so if original
-        // p is odd, then finally it will face the length 1
+        // p == 1 is a special case
         if (p.length() == 1) {
             return (s.length() == 1)
-                    && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.');
+                    && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0));
         }
 
-        // next char is not '*': must match current character
+        // Next p is not *
         if (p.charAt(1) != '*') {
-            if (s.length() < 1) {
-                return false;
-            } else {
-                return (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')
-                        && isMatch(s.substring(1), p.substring(1));
-            }
+            return (s.length() >= 1
+                    && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'))
+                    && isMatch(s.substring(1), p.substring(1));
         }
-        // next char is *
-        while (s.length() > 0
-               && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')) {
-            if (isMatch(s, p.substring(2))) {
-                return true;
-            }
-            s = s.substring(1);
+
+        // Next p does be *
+        if (isMatch(s, p.substring(2))) {
+            // To look forward first, whether they
+            // can match without the * match.
+            // for example initially, [aaab and a*b], when
+            // reaching the condition [b and a*b], the looking
+            // forward will save you.
+            return true;
+        } else {
+            return (s.length() >= 1
+                    && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'))
+                    && isMatch(s.substring(1), p);
         }
-        return isMatch(s, p.substring(2));
     }
 }
--- test/oj/leet/rem/SolutionTest.java
+++ test/oj/leet/rem/SolutionTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Solution Tester.
  *
@@ -29,12 +32,16 @@ public class SolutionTest {
         org.junit.Assert.assertFalse(solution.isMatch("aa", "a"));
         org.junit.Assert.assertFalse(solution.isMatch("aaa", "a"));
 
-        org.junit.Assert.assertTrue(solution.isMatch("aa", "aa"));
-        org.junit.Assert.assertTrue(solution.isMatch("aa", "a*"));
-        org.junit.Assert.assertTrue(solution.isMatch("aaa", "a*a"));
-        org.junit.Assert.assertTrue(solution.isMatch("ab", ".*"));
-        org.junit.Assert.assertTrue(solution.isMatch("aab", "c*a*b"));
-        org.junit.Assert.assertTrue(solution.isMatch("a", "."));
+        assertTrue(solution.isMatch("aa", "aa"));
+        assertTrue(solution.isMatch("aa", "a*"));
+        assertTrue(solution.isMatch("aaa", "a*a"));
+        assertTrue(solution.isMatch("ab", ".*"));
+        assertTrue(solution.isMatch("aab", "c*a*b"));
+        assertTrue(solution.isMatch("a", "."));
+        assertTrue(solution.isMatch("bbba", ".*a*a"));
+        assertFalse(solution.isMatch("ab", ".*c"));
+        assertFalse(solution.isMatch("aa", "."));
+        assertFalse(solution.isMatch("acaabbaccbbacaabbbb", "a*.*b*.*a*aa*a*"));
 
     }
 }
