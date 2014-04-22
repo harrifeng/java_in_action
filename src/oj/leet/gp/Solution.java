bb9ddbf77c3ba3dc5b23be894be4fe21811b1356
Harri Feng <harrifeng@gmail.com>
2014-04-22 21:33:32 +0800

    generate parentheses repeated, backtrace always need "BACK"!

--- src/oj/leet/gp/Solution.java
+++ src/oj/leet/gp/Solution.java
@@ -3,28 +3,26 @@ package oj.leet.gp;
 import java.util.ArrayList;
 
 public class Solution {
-
     public ArrayList<String> generateParenthesis(int n) {
         ArrayList<String> ret = new ArrayList<String>();
-        genPar(new StringBuffer(), n, n, ret);
+        genP(n, n, new StringBuilder(), ret);
         return ret;
     }
 
-    private void genPar(StringBuffer sbf, int left, int right, ArrayList<String> result) {
-        if (left == 0 && right == 0) {
-            result.add(sbf.toString());
+    private void genP(int left, int right, StringBuilder tmp, ArrayList<String> result) {
+        if (left == 0 && left == right) {
+            result.add(tmp.toString());
             return;
         }
-
         if (left > 0) {
-            sbf.append('(');
-            genPar(sbf, left - 1, right, result);
-            sbf.deleteCharAt(sbf.length()-1);
+            tmp.append('(');
+            genP(left - 1, right, tmp, result);
+            tmp.deleteCharAt(tmp.length() - 1);
         }
-        if (right > 0 && right > left) {
-            sbf.append(')');
-            genPar(sbf,left, right -1, result);
-            sbf.deleteCharAt(sbf.length()-1);
+        if (right > left) {
+            tmp.append(')');
+            genP(left, right - 1, tmp, result);
+            tmp.deleteCharAt(tmp.length() - 1);
         }
     }
 }
