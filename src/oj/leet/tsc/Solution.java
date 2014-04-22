ff81e750e5e2766605affaef1b085437ff84f22c
Harri Feng <harrifeng@gmail.com>
2014-04-22 11:14:57 +0800

    3sum closet repeated,no need to check redundancy.

--- src/oj/leet/tsc/Solution.java
+++ src/oj/leet/tsc/Solution.java
@@ -3,32 +3,31 @@ package oj.leet.tsc;
 import java.util.Arrays;
 
 public class Solution {
+
     public int threeSumClosest(int[] num, int target) {
-        Arrays.sort(num);
-        int minV = Integer.MAX_VALUE;
         if (num.length < 3) {
-            return minV;
+            return 0;
         }
         int minSum = Integer.MAX_VALUE;
+        int minV = Integer.MAX_VALUE;
+        Arrays.sort(num);
         for (int i = 0; i < num.length; i++) {
-            if (i > 0 && num[i] == num[i - 1]) {
-                continue;
-            }
+
             int beg = i + 1;
             int end = num.length - 1;
             while (beg < end) {
                 int sum = num[i] + num[beg] + num[end];
-
-                if (sum == target) {
+                if (sum - target == 0) {
                     return target;
-                } else if (sum < target) {
-                    beg++;
-                } else {
-                    end--;
                 }
                 if (minV > Math.abs(sum - target)) {
                     minV = Math.abs(sum - target);
                     minSum = sum;
+                }
+                if (sum - target < 0) {
+                    beg++;
+                } else {
+                    end--;
                 }
             }
         }
